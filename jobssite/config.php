090f31935e562efb11620d15414403eee4891b91
Designables <rainbowy007@yahoo.com>
2018-11-30 15:42:06 +0500

    jb files
    jb files added

---
+++ jobssite/config.php
@@ -0,0 +1,117 @@
+<?php
+// GENERAL SITE SETTINGS
+
+
+// THEMES
+$theme = 'flatly';
+$themeswitcher = 'off';
+$smooth_scroll = 'off';
+$show_categories = true;
+$show_locations = true;
+$jobaddlink = true;
+
+
+// ADVERTISING & AFFILIATE
+
+$simpleyhired_publisher_id = '53753';
+
+$indeed_publisher_id = "17083005191573";
+$indeed_channel = 'jobsaggregator';
+
+$careerjet_publisher_id = "4a5b3595b250fa9f7567c9ed63d5e0b4";
+
+$adsenseads = '';
+
+//Redirect to Default Country
+$default_country = 'off'; // on or off
+
+// GOOGLE ANALYTICS
+$statistics_code = "";
+
+// ADDTHIS PROFILES
+
+$addthis = "";
+
+// LANGUAGE
+$locale = 'en-US';
+$language_switcher = 'on';
+
+// SEARCH SETTINGS
+$limit_listings = "10"; // Limit search listings per source
+$default_keyword = ""; // Set Deafult Niche keyword. ie: Developer
+$default_location = ""; // Set Deafult Location
+
+
+// GLOBAL LOCALE CONFIG
+
+$_locales = array(
+'VN' => 'vi-VN',
+'NL' => 'nl-NL',
+'US' => 'en-US',
+'UK' => 'uk-UK',
+'TR' => 'tr-TR',
+'CZ' => 'cs-CZ',
+'DK' => 'da-DK',
+'AT' => 'de-AT',
+'CH' => 'de-CH',
+'DE' => 'de-DE',
+'AE' => 'en-AE',
+'AU' => 'en-AU',
+'CA' => 'en-CA',
+'CN' => 'en-CN',
+'HK' => 'en-HK',
+'IE' => 'en-IE',
+'IN' => 'en-IN',
+'MY' => 'en-MY',
+'NZ' => 'en-NZ',
+'OM' => 'en-OM',
+'PH' => 'en-PH',
+'PK' => 'en-PK',
+'QA' => 'en-QA',
+'SG' => 'en-SG',
+'GB' => 'en-GB',
+'ZA' => 'en-ZA',
+'TW' => 'en-TW',
+'VN' => 'en-VN',
+'AR' => 'es-AR',
+'BO' => 'es-BO',
+'CL' => 'es-CL',
+'CR' => 'es-CR',
+'DO' => 'es-DO',
+'EC' => 'es-EC',
+'ES' => 'es-ES',
+'GT' => 'es-GT',
+'MX' => 'es-MX',
+'PA' => 'es-PA',
+'PE' => 'es-PE',
+'PR' => 'es-PR',
+'PY' => 'es-PY',
+'UY' => 'es-UY',
+'VE' => 'es-VE',
+'FI' => 'fi-FI',
+'CA' => 'fr-CA',
+'BE' => 'fr-BE',
+'CH' => 'fr-CH',
+'FR' => 'fr-FR',
+'LU' => 'fr-LU',
+'MA' => 'fr-MA',
+'HU' => 'hu-HU',
+'IT' => 'it-IT',
+'JP' => 'ja-JP',
+'KR' => 'ko-KR',
+'BE' => 'nl-BE',
+'NO' => 'no-NO',
+'PL' => 'pl-PL',
+'PT' => 'pt-PT',
+'BR' => 'pt-BR',
+'RU' => 'ru-RU',
+'UA' => 'ru-UA',
+'SE' => 'sv-SE',
+'SK' => 'sk-SK',
+'UA' => 'uk-UA',
+'CN' => 'zh-CN'
+);
+
+
+// END
+?>
